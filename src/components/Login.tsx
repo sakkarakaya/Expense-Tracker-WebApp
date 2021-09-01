e9ad03e7d98883149b6d0c051651037012b5a84d
sakkarakaya <sakkarakaya@icloud.com>
2021-09-01 20:12:24 +0200

    added login component

---
+++ src/components/Login.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { Form, Input, Button, Checkbox } from "antd";
+import { useHistory } from "react-router-dom";
+import api from "../utils/api";
+
+const Login = () => {
+  const history = useHistory();
+  const onFinish = async (values: any) => {
+    console.log("Success:", values);
+    try {
+      await api.post("/users/login", values);
+      history.push("/");
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <Form
+      name='basic'
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete='off'
+    >
+      <Form.Item
+        label='Username'
+        name='username'
+        rules={[{ required: true, message: "Please input your username!" }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label='Password'
+        name='password'
+        rules={[{ required: true, message: "Please input your password!" }]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name='remember'
+        valuePropName='checked'
+        wrapperCol={{ offset: 8, span: 16 }}
+      >
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type='primary' htmlType='submit'>
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default Login;
