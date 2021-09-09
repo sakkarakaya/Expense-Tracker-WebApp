d5f20abd2ad667d954918a77ca98e394f677b2be
sakkarakaya <sakkarakaya@icloud.com>
2021-09-09 12:36:50 +0200

    added token to local storage

--- src/store/actions/userAction.ts
+++ src/store/actions/userAction.ts
@@ -1,11 +1,12 @@
 import { LoginForm, User, UserDispatch } from "../../types/user";
-import api from "../../utils/api"
+import api from "../../utils/api";
 
 export const login = (creds: LoginForm) => async (dispatch: UserDispatch) => {
     dispatch({type: "LOGIN_ERROR"});
     try{
         const response = await api.post<User>("/users/login", creds);
-        dispatch({type: "LOGIN_SUCCESS", payload: response.data})
+        dispatch({type: "LOGIN_SUCCESS", payload: response.data});
+        localStorage.setItem("token", response.data.token)
     } catch {
         dispatch({type: "LOGIN_ERROR"})
     }
