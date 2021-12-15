3ac38271e7f45384ffeca8920e69ef09a96c4a34
sakkarakaya <sakkarakaya@icloud.com>
2021-12-15 10:42:01 +0100

    added show error to register

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -2,32 +2,34 @@ import React from "react";
 import { Form, Input, Button } from "antd";
 import api from "../utils/api";
 import { useHistory } from "react-router-dom";
+import showError from "../utils/showError";
 
 const layout = {
-  labelCol: { span: 8 },
+  labelCol: { span: 4 },
   wrapperCol: { span: 16 },
 };
 
-const validateMessages = {
-  required: "${label} is required!",
-  types: {
-    email: "${label} is not a valid email!",
-    number: "${label} is not a valid number!",
-  },
-  number: {
-    range: "${label} must be between ${min} and ${max}",
-  },
-};
+// const validateMessages = {
+//   required: "${label} is required!",
+//   types: {
+//     email: "${label} is not a valid email!",
+//     number: "${label} is not a valid number!",
+//   },
+//   number: {
+//     range: "${label} must be between ${min} and ${max}",
+//   },
+// };
 
 function SignUp() {
   const history = useHistory();
 
   const onFinish = async (values: any) => {
     try {
-      await api.post("/users/register", values);
+      await api().post("/users/register", values);
       history.push("/login", { newSignUp: true });
     } catch (error) {
       console.log({ error });
+      showError((error as any).response.data.errorMessage);
     }
   };
   return (
@@ -35,7 +37,7 @@ function SignUp() {
       {...layout}
       name='nest-messages'
       onFinish={onFinish}
-      validateMessages={validateMessages}
+      // validateMessages={validateMessages}
     >
       <Form.Item name='username' label='Username' rules={[{ required: true }]}>
         <Input />
