697377e466f14fbebcc350de5613e48389708ed2
sakkarakaya <sakkarakaya@icloud.com>
2021-12-21 12:52:06 +0100

    added category crud

--- src/store/reducers/categoryReducer.ts
+++ src/store/reducers/categoryReducer.ts
@@ -14,6 +14,24 @@ const categoryReducer = (state: CategoryState = defaultState, action: CategoryAc
             return {...state, loading: false, data: action.payload}
         case "GET_ERROR":
             return {...state, loading: false, error:"get error"}
+        case "ADD_START":
+            return {...state, loading: true, error: ""}
+        case "ADD_SUCCESS":
+            return {...state, loading: false, data: [action.payload, ...state.data]}
+        case "ADD_ERROR":
+            return {...state, loading: false, error:"add error"}
+        case "EDIT_START":
+            return {...state, loading: true, error: ""}
+        case "EDIT_SUCCESS":
+            return {...state, loading: false, data: state.data.map(category => category.id === action.payload.id ? action.payload : category)}
+        case "EDIT_ERROR":
+            return {...state, loading: false, error:"edit error"}
+        case "DELETE_START":
+            return {...state, loading: true, error: ""}
+        case "DELETE_SUCCESS":
+            return {...state, loading: false, data: state.data.filter(category => category.id !== action.payload)}
+        case "DELETE_ERROR":
+            return {...state, loading: false, error:"delete error"}
         default:
             return state;
     }
